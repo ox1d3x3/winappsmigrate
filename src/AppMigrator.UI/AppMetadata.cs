df379bcf99b0ac49b0f5ebd0e5c0376be6d978be
Mahabub Subhany <66554413+ox1d3x3@users.noreply.github.com>
2026-03-28 14:52:45 +0800

    fixed code

--- src/AppMigrator.UI/AppMetadata.cs
+++ src/AppMigrator.UI/AppMetadata.cs
@@ -3,7 +3,7 @@ namespace AppMigrator.UI;
 public static class AppMetadata
 {
     public const string ProductName = "Win Apps Migrator";
-    public const string Version = "0.0.15";
+    public const string Version = "0.0.16";
     public const string Author = "Ox1d3x3";
     public const string ProjectUrl = "https://github.com/ox1d3x3/winappsmigrate/releases";
     public const string GitHubLatestReleaseApiUrl = "https://api.github.com/repos/ox1d3x3/winappsmigrate/releases/latest";
